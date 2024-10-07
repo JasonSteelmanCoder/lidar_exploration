ea5d93c2576afad5e387f06a360bddf0d0e295a1
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-07 09:14:38 -0400

    trim redundant code

--- find_ground.r
+++ find_ground.r
@@ -25,30 +25,6 @@ fuel.points <- Filter(function(x) x > 0,  attributes(norm.las)$data$Z)
 boxplot(x = fuel.points, main = 'Height Distribution of Fuel Bed',  ylab = "heights (m)")
 
 
-# find the center of the fuel pile 
-#df = data.frame(attributes(norm.las)$data$Classification, attributes(norm.las)$data$X, attributes(norm.las)$data$Y, attributes(norm.las)$data$Z, attributes(norm.las)$data$Zref)
-#names(df) <- c("Classification", "X", "Y", "Z", "Zref")
-#fuel.df <- df[df$"Classification" == 0, ]
-
-#x.center <- median(fuel.df$X)
-#y.center <- median(fuel.df$Y)
-#print(x.center)
-#print(y.center)
-
-
-# find points that are within 0.5m of the center of the fuel pile
-#fuel.df[["dist_from_ctr"]] <- sqrt((fuel.df$X - x.center)^2 + (fuel.df$Y - y.center)^2)
-#circle.df <- fuel.df[fuel.df$dist_from_ctr <= 0.5, ]
-
-
-# estimate volume within 0.5m of the center of the fuel pile
-#median.height <- median(circle.df$Z)
-#print(median.height)
-
-#volume <- median.height * pi * 0.25
-#print(volume)
-
-
 # rasterize the top of the fuel bed
 fuel.raster <- rasterize_canopy(norm.las, res = 0.004, algorithm = dsmtin())
 plot(fuel.raster)
