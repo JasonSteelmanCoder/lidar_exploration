9fafc465528c3627e0aa1769aa773bb4da1663f3
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-07 13:20:49 -0400

    housekeeping

--- script2.py
+++ script2.py
@@ -3,10 +3,5 @@ import cccorelib
 import numpy as np
 
 CC = pycc.GetInstance()
-dish = pycc.ccDish(0.5, 0.02, 0)
-dish.showColors(True)
 
-print(dish.getTransformation())
-
-CC.addToDB(dish)
-CC.updateUI()
+print(cccorelib.GeometricalAnalysisTools.ComputeGravityCenter(CC.ccGenericPointCloud))
