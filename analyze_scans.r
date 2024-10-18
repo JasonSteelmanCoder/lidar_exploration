93ccb68ab03be9c51f5afc910e8ac952565b95ca
Jason Steelman <jasonsteelmancoder@gmail.com>
2024-10-18 12:52:07 -0400

    crop and measure scans

--- analyze_scans.r
+++ analyze_scans.r
@@ -17,13 +17,47 @@ input.csv <- "C:/Users/js81535/Desktop/lidar_exploration/names_and_masses.csv"
 # USER: set the location where the output should go
 output.folder <- "C:/Users/js81535/Desktop/lidar_exploration/autoclipped_scans_for_analysis"
 
-# the computer gets the information it needs from the folder and csv file
+# grab data from the folder and csv file
 input.file.names <- list.files(path = input.folder, full.names = FALSE)
 input.data <- read.csv(input.csv)
 
+# initialize output data frame
+output.data <- data.frame(input.data)
 
-# loop through all of the files in the input folder to crop them
-for (file.name in input.file.names) {
+output.data$min_height_pre <- NA
+output.data$q1_height_pre <- NA
+output.data$median_height_pre <- NA
+output.data$q3_height_pre <- NA
+output.data$max_height_pre <- NA
+output.data$volume_pre <- NA
+output.data$bulk_density_pre <- NA
+
+output.data$min_height_post <- NA
+output.data$q1_height_post <- NA
+output.data$median_height_post <- NA
+output.data$q3_height_post <- NA
+output.data$max_height_post <- NA
+output.data$volume_post <- NA
+output.data$bulk_density_post <- NA
+
+output.data$consumption_by_volume <- NA
+output.data$consumption_by_mass <- NA
+
+
+
+
+
+
+# loop through the raw pre and post files to crop them
+all.files <- c(input.data$pre_file_name, input.data$post_file_name)
+
+for (file.name in all.files) {
+  # check that the file exists in the folder
+  if (!file.exists(file.path(input.folder, file.name))) {
+    cat("the file \'", file.name, "\' is not in the input folder.\nEnsure that the folder has pre and post burn .las files with names that match the ones in the pre_file_name and post_file_name columns of the input csv.\n")
+    next
+  }
+
   # load the scan
   las <- readLAS(file.path(input.folder, file.name))
   
@@ -55,7 +89,7 @@ for (file.name in input.file.names) {
   #plot(platform.las)
   
   # write the cropped scan to a file in the output folder.
-  cat("processing ", file.name)  
+  cat("processing ", file.name, '...\n')  
   if (grepl('_pre\\.las$', file.name)) {
     new.file.name <- sub("_pre\\.las$", "_autoclipped_pre.las", file.name)
   } else if (grepl('_post\\.las$', file.name)) {
@@ -63,11 +97,78 @@ for (file.name in input.file.names) {
   } else {
     new.file.name <- file.name
   }
-  cat("stored as ", new.file.name)
+  cat("stored as ", new.file.name, '\n')
   writeLAS(platform.las, file.path(output.folder, new.file.name))
 }
 
 
 
-# find 
 
+
+
+# define a function to find height distributions and volumes
+find.hdv <- function(file.name, pre.post, output.row) {
+  # load the scan
+  las <- readLAS(file.path(output.folder, file.name))
+  
+  # classify noise points and trim them
+  las <- classify_noise(las, sor(k=10, m=8))
+  las <- filter_poi(las, Classification != 18)
+  
+  # classify ground and non-ground points
+  mycsf <- csf(FALSE, class_threshold = 0.009, rigidness = 3)
+  las <- classify_ground(las, mycsf)
+  
+  # normalize heights from the ground
+  norm.las <- normalize_height(las, tin())
+  
+  # find height distribution and add it to the output data frame
+  fuel.points <- Filter(function(x) x > 0,  attributes(norm.las)$data$Z)
+  min.height <- min(fuel.points)
+  q1.height <- quantile(fuel.points, probs = 0.25, names=FALSE)[1]
+  median.height <- median(fuel.points)
+  q3.height <- quantile(fuel.points, probs = 0.75, names=FALSE)[1]
+  max.height <- max(fuel.points)
+  # . . . . . . . 
+  
+  
+  # rasterize the top of the fuel bed
+  fuel.raster <- rasterize_canopy(norm.las, res = 0.004, algorithm = dsmtin())
+  
+  # get all of the individual pixels in the raster
+  fuel.matrix <- as.matrix(fuel.raster)
+  
+  # make an empty list. Then, populate it with the volumes for each pixel
+  uprights <- c()
+  for (point in fuel.matrix) {
+    # we only want points that have positive height
+    if (!is.na(point) && point > 0) {
+      upright.volume <- point * 0.000016    # multiply by the area of each pixel (which is the resolution squared)
+      uprights <- append(uprights, upright.volume)      
+    }
+  }
+  total.volume <- sum(uprights)
+  
+  print(pre.post)
+  print(min.height)
+  print(q1.height)
+  print(median.height)
+  print(q3.height)
+  print(max.height)
+  print(total.volume)  
+}
+
+# find height distribution and volume for each file and add it to the output data frame
+for (i in 1:nrow(input.data)) {
+  # grab names from the input csv
+  pre.name <- input.data[i, "pre_file_name"]
+  post.name <- input.data[i, "post_file_name"]
+  
+  # update the names to match the new, processed scans
+  pre.name <- sub("_pre\\.las$", "_autoclipped_pre.las", pre.name)
+  post.name <- sub("_post\\.las$", "_autoclipped_post.las", post.name)
+  
+  # find height distributions and volumes and add them to the output data frame
+  find.hdv(pre.name, 'pre', i)
+  find.hdv(post.name, 'post', i)
+}
